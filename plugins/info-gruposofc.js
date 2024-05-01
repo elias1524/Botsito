b4a100896367ad1f151fa8a2c0557d8016c0fdf1
Ivan <162828351+elias1524@users.noreply.github.com>
2024-05-01 00:18:22 -0400

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,15 +1,15 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `*Hola 👋🏻, Bienvenido a los grupos oficiales, te invito a unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de LoliBot-MD 😸*
+let info = `*Hola 👋🏻, Bienvenido a los grupos oficiales, te invito a unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de Goku-Black-Bot-MD*
 
 ➤ Grupos oficiales del bot:
   1) *${nn}*
   
   2) *${nnn}*
 
-➤ Grupo del Colaboracion LoliBot, GataBot-MD
+➤ Grupo oficial de Goku-Black-Bot-MD 
  *${nnnt}*
 
-➤ Grupo del colaboración LoliBot, DorratBot-MD
+➤ Grupo oficial de Goku-Black-Bot-MD 
 *${nnnt2}*
 
 ➤ Grupo del col 3 (sin limite)
@@ -19,17 +19,17 @@ let info = `*Hola 👋🏻, Bienvenido a los grupos oficiales, te invito a unete
 *${nna}*
  
 ➤ *¡Visita todos los enlaces oficiales en un único lugar!*
-https://atom.bio/lolibot
+https://atom.bio/baileybotmd
 
  ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 
-*⇶⃤꙰𝑬𝒏𝒍𝒂𝒄𝒆 𝒍𝒐𝒍𝒊𝒃𝒐𝒕ꦿ⃟⃢*
+*Enlace de Goku-Black-Bot-MD*
 *${nnnttt}*`.trim() 
 conn.reply(m.chat, info, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: `${wm}`,
 body: '', previewType: 0, thumbnail: imagen2, sourceUrl: nna}}})
-//conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '𝙏𝙝𝙚-𝙇𝙤𝙡𝙞𝘽𝙤𝙩-𝙈𝘿', 'status@broadcast')
+//conn.fakeReply(m.chat, info, '0@s.whatsapp.net', 'Goku-Black-Bot-MD', 'status@broadcast')
 }
 handler.command = /^linkgc|grupos|gruposgatabot|gatabotgrupos|gruposdegatabot|groupofc|gruposgb|grupogb|groupgb$/i
 export default handler
