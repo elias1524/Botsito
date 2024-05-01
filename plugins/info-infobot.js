e098bf5e79e3fab9f5fac909568587c97befd23c
Ivan <162828351+elias1524@users.noreply.github.com>
2024-05-01 08:30:06 -0400

    Actualizar info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -1,78 +1,87 @@
-import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
-import os from 'os'
-import util from 'util'
-import sizeFormatter from 'human-readable'
-import MessageType from '@whiskeysockets/baileys'
-import fs from 'fs'
-import { performance } from 'perf_hooks'
+import { generateWAMessageFromContent } from "@whiskeysockets/baileys";
+import os from "os";
+import util from "util";
+import sizeFormatter from "human-readable";
+import MessageType from "@whiskeysockets/baileys";
+import fs from "fs";
+import { performance } from "perf_hooks";
+const handler = async (m, { conn, usedPrefix }) => {
+  const _uptime = process.uptime() * 1000;
+  const uptime = clockString(_uptime);
+  const totalreg = Object.keys(global.db.data.users).length;
+  const chats = Object.entries(conn.chats).filter(
+    ([id, data]) => id && data.isChats,
+  );
+  const groupsIn = chats.filter(([id]) => id.endsWith("@g.us"));
+  const groups = chats.filter(([id]) => id.endsWith("@g.us"));
+  const used = process.memoryUsage();
+  const { restrict, antiCall, antiprivado, modejadibot } =
+    global.db.data.settings[conn.user.jid] || {};
+  const { autoread, gconly, pconly, self } = global.opts || {};
+  const old = performance.now();
+  const neww = performance.now();
+  const speed = neww - old;
+    await conn.sendMessage(m.chat, { react: { text: '🥀', key: m.key } })
+  const info = `
 
-var handler = async (m, { conn, usedPrefix }) => {
-
-let _uptime = process.uptime() * 1000
-let uptime = clockString(_uptime) 
-let totalreg = Object.keys(global.db.data.users).length
-
-const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
-const groups = chats.filter(([id]) => id.endsWith('@g.us'))
-const used = process.memoryUsage()
-const { restrict, antiCall, antiprivado, modejadibot } = global.db.data.settings[conn.user.jid] || {}
-const { autoread, gconly, pconly, self } = global.opts || {}
-
-let old = performance.now()
-let neww = performance.now()
-let speed = neww - old
-
-let info = `⬡ *Creador*
-Azami
-
-⬡ *Contacto* 
-${ig}
-
-⬡ *Versión actual*
- ${vs}
-
-⬡ *Prefijo*
-!
-
-⬡ *Chats privados*
-${chats.length - groups.length}
-
-⬡ *Chats grupales*
-${groups.length}
-
-⬡ *Todos los chats*
-${chats.length}
-
-⬡ *Actividad*
-${uptime}
-
-⬡ *Usuarios*
-${totalreg}
-
-⬡ *Velocidad*
-${speed}
-
-⬡ *Autoread*
-${autoread ? 'Habilitado' : 'Deshabilitado'}
-
-⬡ *Restrict*
-${restrict ? 'Habilitado' : 'Deshabilitado'}`
-
- conn.sendMessage(m.chat, { text: info, contextInfo: { externalAdReply: { title: '', body: '🚩 Curiosity Info', thumbnail: imagen2, sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
-
-}
-handler.help = ['infobot']
-handler.tags = ['info']
-handler.command = /^(infobot|Infobot)$/i
-
+╭━━━━━━･❪ ❁ ❫ ･━━━━━━
+│➸ *⏰️Actividad*
+│➸ *${uptime}*
+│
+│•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•
+│
+│➸ *🧸Chats*
+│➸ 𝘊𝘩𝘢𝘵𝘴 𝘗𝘳𝘪𝘷𝘢𝘥𝘰𝘴: *${chats.length - groups.length}*
+│➸ 𝘊𝘩𝘢𝘵𝘴 𝘋𝘦 𝘎𝘳𝘶𝘱𝘰𝘴: *${groups.length}* 
+│➸ 𝘊𝘩𝘢𝘵𝘴 𝘛𝘰𝘵𝘢𝘭𝘦𝘴: *${chats.length}* 
+│
+│•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•
+│
+│➸ 𝙓𝙚𝙣𝙤𝙂𝙤𝙠𝙪𝘽𝙡𝙖𝙘𝙠🧸📍
+╰━━━━━━･❪ ❁ ❫ ･━━━━━━❖`.trim();
+  const doc = [
+    "pdf",
+    "zip",
+    "vnd.openxmlformats-officedocument.presentationml.presentation",
+    "vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "vnd.openxmlformats-officedocument.wordprocessingml.document",
+  ];
+  const document = doc[Math.floor(Math.random() * doc.length)];
+  const Message = {
+    document: { url: `https://github.com/diegojadibot/SakuraBotLite-MD` },
+    mimetype: `application/${document}`,
+    fileName: `Sakura Bot Lite 🌸`,
+    fileLength: 99999999999999,
+    pageCount: 200,
+    contextInfo: {
+      forwardingScore: 200,
+      isForwarded: true,
+      externalAdReply: {
+        mediaUrl: "https://github.com/diegojadibot/SakuraBotLite-MD",
+        mediaType: 2,
+        previewType: "pdf",
+        title: "Tᴜ Mᴇᴊᴏʀ Bᴏᴛ!",
+        body: wm,
+        thumbnail: imagen6,
+        sourceUrl: "https://atom.bio/sakuralite",
+      },
+    },
+    caption: info,
+    footer: wm,
+    headerType: 6,
+  };
+  conn.sendMessage(m.chat, Message, { quoted: m });
+};
+handler.help = ["infobot", "speed"];
+handler.tags = ["info", "tools"];
+handler.command = /^(ping|speed|infobot)$/i;
 handler.register = true
-
-export default handler
+export default handler;
 
 function clockString(ms) {
-let h = Math.floor(ms / 3600000)
-let m = Math.floor(ms / 60000) % 60
-let s = Math.floor(ms / 1000) % 60
-console.log({ms,h,m,s})
-return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
+  const h = Math.floor(ms / 3600000);
+  const m = Math.floor(ms / 60000) % 60;
+  const s = Math.floor(ms / 1000) % 60;
+  console.log({ ms, h, m, s });
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(":");
+}
