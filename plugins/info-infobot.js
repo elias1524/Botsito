9fc4e7b2b23f43edf0209cc0390f58e08960a696
Ivan <162828351+elias1524@users.noreply.github.com>
2024-05-01 08:28:11 -0400

    Actualizar info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -1,88 +1,78 @@
-import db from '../lib/database.js'
-import { cpus as _cpus, totalmem, freemem, platform, hostname, version, release, arch } from 'os'
-import speed from 'performance-now'
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
+import os from 'os'
+import util from 'util'
+import sizeFormatter from 'human-readable'
+import MessageType from '@whiskeysockets/baileys'
+import fs from 'fs'
 import { performance } from 'perf_hooks'
-import { sizeFormatter } from 'human-readable'
 
-let format = sizeFormatter({
-    std: 'JEDEC',
-    decimalPlaces: 2,
-    keepTrailingZeroes: false,
-    render: (literal, symbol) => `${literal} ${symbol}B`,
-})
+var handler = async (m, { conn, usedPrefix }) => {
 
-let handler = async (m, { conn, usedPrefix }) => {
-   let bot = global.db.data.settings[conn.user.jid]
-   let _uptime = process.uptime() * 1000
-   let uptime = (_uptime).toTimeString()
-   let totalreg = Object.keys(global.db.data.users).length
-   const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-   const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
-   const used = process.memoryUsage()
-   const cpus = _cpus().map(cpu => {
-      cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-      return cpu
-   })
-   const cpu = cpus.reduce((last, cpu, _, { length }) => {
-      last.total += cpu.total
-      last.speed += cpu.speed / length
-      last.times.user += cpu.times.user
-      last.times.nice += cpu.times.nice
-      last.times.sys += cpu.times.sys
-      last.times.idle += cpu.times.idle
-      last.times.irq += cpu.times.irq
-      return last
-   }, {
-      speed: 0,
-      total: 0,
-      times: {
-         user: 0,
-         nice: 0,
-         sys: 0,
-         idle: 0,
-         irq: 0
-      }
-   })
-   let _muptime
-   if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-         process.once('message', resolve)
-         setTimeout(resolve, 1000)
-      }) * 1000
-   }
-   let timestamp = speed()
-   let latensi = speed() - timestamp
-   let teks = ` –  *I N F O  B O T*
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime) 
+let totalreg = Object.keys(global.db.data.users).length
 
-┌  ✩  *Creador* : @${owner[0][0].split('@s.whatsapp.net')[0]}
-│  ✩  *Prefijo* : [  ${usedPrefix}  ]
-│  ✩  *Total Comandos* : 402
-│  ✩  *Plataforma* : ${platform()}
-│  ✩  *Servidor* : ${hostname()}
-│  ✩  *RAM* : ${format(totalmem() - freemem())} / ${format(totalmem())}
-│  ✩  *FreeRAM* : ${format(freemem())}
-│  ✩  *Speed* : ${latensi.toFixed(4)} ms
-│  ✩  *Uptime* : ${uptime}
-│  ✩  *Modo* : ${bot.public ? 'Privado' : 'Publico'}
-└  ✩  *Registrados* : ${totalreg} Usuarios
+const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
+const groups = chats.filter(([id]) => id.endsWith('@g.us'))
+const used = process.memoryUsage()
+const { restrict, antiCall, antiprivado, modejadibot } = global.db.data.settings[conn.user.jid] || {}
+const { autoread, gconly, pconly, self } = global.opts || {}
 
+let old = performance.now()
+let neww = performance.now()
+let speed = neww - old
 
- –  *I N F O  C H A T*
+let info = `⬡ *Creador*
+Azami
 
-┌  ✩  *${groupsIn.length}* Chats en Grupos
-│  ✩  *${groupsIn.length}* Grupos Unidos
-│  ✩  *${groupsIn.length - groupsIn.length}* Grupos Salidos
-│  ✩  *${chats.length - groupsIn.length}* Chats Privados
-└  ✩  *${chats.length}* Chats Totales
+⬡ *Contacto* 
+${ig}
 
-*≡  _NodeJS Uso de memoria_*
-${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}`
-await conn.reply(m.chat, teks, m, { contextInfo: { mentionedJid: [owner[0][0] + '@s.whatsapp.net'], externalAdReply: { mediaUrl: false, mediaType: 1, description: false, title: '↷✦╎Info - Bot╎⭐˖ ⸙',body: false, previewType: 0, thumbnail: miniurl, sourceUrl: ''}}})
+⬡ *Versión actual*
+ ${vs}
+
+⬡ *Prefijo*
+!
+
+⬡ *Chats privados*
+${chats.length - groups.length}
+
+⬡ *Chats grupales*
+${groups.length}
+
+⬡ *Todos los chats*
+${chats.length}
+
+⬡ *Actividad*
+${uptime}
+
+⬡ *Usuarios*
+${totalreg}
+
+⬡ *Velocidad*
+${speed}
+
+⬡ *Autoread*
+${autoread ? 'Habilitado' : 'Deshabilitado'}
+
+⬡ *Restrict*
+${restrict ? 'Habilitado' : 'Deshabilitado'}`
+
+ conn.sendMessage(m.chat, { text: info, contextInfo: { externalAdReply: { title: '', body: '🚩 Curiosity Info', thumbnail: imagen2, sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
+
 }
+handler.help = ['infobot']
+handler.tags = ['info']
+handler.command = /^(infobot|Infobot)$/i
 
-handler.help = ['info']
-handler.tags = ['main']
-handler.command = ['info', 'infobot']
+handler.register = true
 
 export default handler
+
+function clockString(ms) {
+let h = Math.floor(ms / 3600000)
+let m = Math.floor(ms / 60000) % 60
+let s = Math.floor(ms / 1000) % 60
+console.log({ms,h,m,s})
+return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
