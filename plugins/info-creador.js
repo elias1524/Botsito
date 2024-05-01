0b515d456a42a085ed710b0e57fcb73c222e4dee
Ivan <162828351+elias1524@users.noreply.github.com>
2024-04-30 21:11:57 -0400

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,14 +1,23 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), estilo, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
+var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let pp = gataImg.getRandom()	
+const cat = `
+╭ ❖ ── ✦ ── ✦ ── ❖ ──╗
+│
+│ ✥𝗜𝗩𝗔𝗡 𝗠𝗢𝗗𝗭✥ 
+│ *Wa.me/595972,157130*
+│  
+│*GRUPO OFICIAL DE GOKU BLACK MD*
+│*https://chat.whatsapp.com/GkajTNHTklT7RJ1bzsJYNK*
+│
+│ANGEL-OFC - ASISTENCIA
+│*${asistencia}*
+╰─ ❖ ── ✦ ── ✦ ── ❖ ──╝`
 
-
+await conn.sendFile(m.chat, pp, 'gata.mp4', cat, fkontak)
 }
-
-handler.help = ['creador']
+handler.help = ['owner', 'creator']
 handler.tags = ['info']
-
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
-
+handler.command = /^(owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
 
 export default handler
